296b8c00c3d7948c3176a5c3ddaf64ed4e9956d4
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-08-29 12:24:41 -0300

    Adicionado a interface IUser
    - IUser define os contratos para a criação de um usuário no sistema, possuindo métodos e atributos para exibição no console do sistema.

---
+++ Interfaces/Entities/IUser.cs
@@ -0,0 +1,11 @@
+﻿namespace LojaVirtual.Interfaces.Entities
+{
+    internal interface IUser
+    {
+        public string Name { get; set; }
+        public decimal DebitCardBalance { get; set; }
+        public decimal CreditCardBalance { get; set; }
+
+        void ShowDetails();
+    }
+}
