7834af1cdad05cc272e6c25dce40bd6822763cc9
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-08-29 12:22:35 -0300

    Adicionada o enumerador EPaymentType
    - EPaymentType é um enumerador que representa os tipos de pagamentos disponíveis no sistema.

---
+++ Enums/EPaymentType.cs
@@ -0,0 +1,15 @@
+﻿namespace LojaVirtual.Enums
+{
+    /// <summary>
+    /// Define os diferentes tipos de pagamentos na aplicação.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="EPaymentType"/> categoriza as formas de pagamentos em tipos distintos, permitindo que o sistema identifique e trate as formas de pagamentos
+    /// baseadas em suas categorias.
+    /// </remarks>
+    internal enum EPaymentType
+    {
+        CartãoDébito = 1,
+        CartãoCrédito = 2
+    }
+}
