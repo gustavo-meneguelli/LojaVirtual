7097e2dc6f51b2855c1106c2c08c27e287326342
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-08-29 12:25:36 -0300

    Adicionada a interface IPayment
    - IPayment define os contratos para a realização de pagamentos no sistema.
    - Contém métodos para validação, exibição de nota fiscal e pagamento.

---
+++ Interfaces/Payment/IPayment.cs
@@ -0,0 +1,28 @@
+﻿namespace LojaVirtual.Interfaces.Payment
+{
+    /// <summary>
+    /// Interface para definir contratos para a realização de pagamentos.
+    /// </summary>
+    /// <remarks>
+    /// Essa interface fornece métodos para a checagem, realização de pagamento e exibição de nota fiscal.
+    /// </remarks>
+    internal interface IPayment
+    {
+        /// <summary>
+        /// Realiza o pagamento.
+        /// </summary>
+        /// <remarks>
+        /// Esse método realiza a validação do pagamento e a exibição da nota fiscal.
+        /// </remarks>
+        void Pay();
+        /// <summary>
+        /// Exibe na tela do console a nota fiscal do produto comprado.
+        /// </summary>
+        void ShowInvoice();
+        /// <summary>
+        /// Realiza a checagem para a verificação do saldo do usuário com o valor do produto correspondente. 
+        /// </summary>
+        /// <returns>Retorna <c>True</c> caso seja possível realizar o pagamento e <c>False</c> caso não for possível realizar o pagamento.</returns>
+        bool CheckUserBalance();
+    }
+}
