870b06eae8f7b1afb368cbcfb1a08e8b3c90dbbf
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-08-29 12:40:06 -0300

    Update Program.cs
    - Instanciada a classe User que implementa a interface IUser.
    - Utilizada como parâmetro a instância de user no MenuFactory como necessário na implementação da mesma.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using LojaVirtual.Factory;
+﻿using LojaVirtual.Entities;
+using LojaVirtual.Factory;
+using LojaVirtual.Interfaces.Entities;
 using LojaVirtual.Interfaces.Factory;
 using LojaVirtual.Interfaces.Menus;
 using LojaVirtual.Interfaces.Products;
@@ -13,9 +15,10 @@ namespace LojaVirtual
         static void Main(string[] args)
         {
             //Inicialização dos objetos
+            IUser user = new User("Gustavo", 99.99m, 129.99m);
             IProductCollectionManager productCollectionManager = new ProductCollectionManager();
             IMenuHelper menuHelper = new MenuHelper();
-            IMenuFactory menuFactory = new MenuFactory(menuHelper);
+            IMenuFactory menuFactory = new MenuFactory(user, menuHelper);
             UserInterface userInterface = new UserInterface(menuHelper, menuFactory, productCollectionManager);
 
             //Inicialização do programa
