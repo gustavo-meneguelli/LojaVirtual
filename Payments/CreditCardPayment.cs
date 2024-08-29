df63ffdedb0e5c21e19729f2c997f84b3efe290d
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-08-29 12:28:07 -0300

    Adicionado a classe CreditCardPayment
    - CreditCardPayment é responsável por gerar o pagamento com o cartão de crédito.
    - CreditCardPayment implementa a interface IPayment com os contratos necessários para realizar um pagamento no sistema.

---
+++ Payments/CreditCardPayment.cs
@@ -0,0 +1,51 @@
+﻿using LojaVirtual.Interfaces.Entities;
+using LojaVirtual.Interfaces.Payment;
+using LojaVirtual.Interfaces.Products;
+
+namespace LojaVirtual.Payments
+{
+    internal class CreditCardPayment : IPayment
+    {
+        private readonly IUser _user;
+        private readonly IProduct _product;
+        private readonly DateTime _paymentDate;
+        public CreditCardPayment(IUser user, IProduct product)
+        {
+            _paymentDate = DateTime.Now;
+            _user = user;
+            _product = product;
+        }
+
+        public bool CheckUserBalance()
+            => _user.CreditCardBalance >= _product.Price;
+
+        public void Pay()
+        {
+            if (CheckUserBalance())
+            {
+                ShowInvoice();
+                _user.CreditCardBalance -= _product.Price;
+            }
+            else
+                Console.WriteLine("Pagamento não realizado por falta de limite no cartão.");
+
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        public void ShowInvoice()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("       NOTA FISCAL         ");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(
+                $"Forma de Pagamento: Cartão de Crédito \n" +
+                $"Produto: {_product.Name} \n" +
+                $"Tipo do produto: {_product.ProductType} \n" +
+                $"Preço do produto R${_product.Price} \n" +
+                $"Nome do titular: {_user.Name} \n" +
+                $"Data do pagamento: {_paymentDate}");
+        }
+    }
+}
