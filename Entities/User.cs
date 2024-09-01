148528ef9a52ff10d4a719f992c5da8a7f2de110
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-09-01 10:10:51 -0300

    Update User.cs
    - Adicionado a propriedade PurchasedProducts que representa uma lista que contém a lista de propriedades da compra realizada pelo usuário.
    
    -Adicionado o método ShowPurchasedProducts que exibe no console todas as informações das compras do usuário.

--- Entities/User.cs
+++ Entities/User.cs
@@ -23,6 +23,11 @@ namespace LojaVirtual.Entities
         public decimal CreditCardBalance { get; set; }
 
         /// <summary>
+        /// Define ou obtém uma Lista de objetos que possui listas de produtos comprados no sistema
+        /// </summary>
+        public List<List<object>> PurchasedProducts { get; set; }
+
+        /// <summary>
         /// Inicializa a instância da classe <see cref="User"/>
         /// </summary>
         /// <param name="name">Nome do usuário</param>
@@ -33,6 +38,7 @@ namespace LojaVirtual.Entities
             Name = name;
             DebitCardBalance = debitCardBalance;
             CreditCardBalance = credidCardBalance;
+            PurchasedProducts = new List<List<object>>();
         }
 
         /// <summary>
@@ -43,6 +49,44 @@ namespace LojaVirtual.Entities
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine($"Usuário: {Name}  \nCartão de Débito R${DebitCardBalance}  \nCartão de Crédito R${CreditCardBalance}");
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Exibe da tela do console uma lista de todos os produtos comprados.
+        /// </summary>
+        /// <remarks>
+        /// Esse método vai exibir todos os produtos comprados pelo usuário, informando o produto, preço, horário da compra, tipo do produto
+        /// e a forma de pagamento.
+        /// </remarks>
+        public void ShowPurchasedProducts()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("                     Produtos Comprados                             ");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Nome do Produto   Preço   Data da Compra   Tipo do Produto   Forma de Pagamento");
+
+            if (PurchasedProducts.Count == 0)
+                Console.WriteLine("Nenhum produto comprado...");
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                for (int i = 0; i < PurchasedProducts.Count; i++)
+                {
+                    Console.WriteLine("-------------------------------------------------------------------------------");
+                    for (int j = 0; j < PurchasedProducts[i].Count; j++)
+                    {
+                        Console.Write($"{PurchasedProducts[i][j]}   ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
