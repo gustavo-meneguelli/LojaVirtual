f9bfcde5c5bd4e5da73bc1f67bc17013c1dd727a
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2024-09-01 10:06:55 -0300

    Create Ebook.cs
    - Adicionado o produto Ebook que implementa a interface IProduct

---
+++ Products/Ebook.cs
@@ -0,0 +1,37 @@
+﻿using LojaVirtual.Enums;
+using LojaVirtual.Interfaces.Products;
+
+namespace LojaVirtual.Products
+{
+    internal class Ebook : IProduct
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public EProductsType ProductType { get; set; }
+        public string Author { get; set; }
+
+        public Ebook(string name, string author, decimal price)
+        {
+            Name = name;
+            Author = author;
+            Price = price;
+            ProductType = EProductsType.Ebook;
+
+        }
+
+        public void ShowDetails()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------------");
+            Console.WriteLine("   Exibindo Detalhes    ");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(
+                $"Titulo: {Name} \n" +
+                $"Autor: {Author} \n" +
+                $"Preço: R${Price} \n" +
+                $"Tipo do produto: {ProductType}");
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
